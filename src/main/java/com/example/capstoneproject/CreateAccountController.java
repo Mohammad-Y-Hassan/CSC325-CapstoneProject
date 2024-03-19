d6054a590747874005d8e6b9e70f3f8be31f70fa
Donovin Bermudez <donovin12131415@gmail.com>
2024-03-19 18:31:26 -0400

    updated startup

---
+++ src/main/java/com/example/capstoneproject/CreateAccountController.java
@@ -0,0 +1,27 @@
+package com.example.capstoneproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class CreateAccountController {
+
+    @FXML
+    private Button CreateAccountButton;
+
+    @FXML
+    private PasswordField PasswordTextField;
+
+    @FXML
+    private Button LogInButton;
+
+    @FXML
+    private TextField FirstNameTextField;
+
+    @FXML
+    private TextField EmailTextField;
+
+
+
+}
---
+++ src/main/java/com/example/capstoneproject/Main.java
@@ -0,0 +1,28 @@
+package com.example.capstoneproject;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/capstoneproject/login.fxml"));
+
+        // Set the root element type programmatically
+        BorderPane root = new BorderPane();
+        loader.setRoot(root);
+
+        // Now load the FXML
+        loader.load();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
