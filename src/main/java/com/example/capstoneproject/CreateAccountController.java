b44ff7de474d8c0a4cd8c3c64af5c762183363f6
mohas <mohassan11714@gmail.com>
2024-03-21 10:16:23 -0400

    First Scene is now 50% complete. When the user enters valid credentials to create an account, he gets transported to the home page.

--- src/main/java/com/example/capstoneproject/CreateAccountController.java
+++ src/main/java/com/example/capstoneproject/CreateAccountController.java
@@ -1,14 +1,27 @@
 package com.example.capstoneproject;
 
+// Used for validating email
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class CreateAccountController {
 
-    @FXML
-    private Button CreateAccountButton;
+    //@FXML
+    //private Button CreateAccountButton;
 
     @FXML
     private PasswordField PasswordTextField;
@@ -22,6 +35,41 @@ public class CreateAccountController {
     @FXML
     private TextField EmailTextField;
 
+    @FXML
+    private Label LogInMessageLabel;
 
+    // Validates email
+    public boolean isValidEmail(String email){
+        // Regular expression pattern for email validation
+        String pattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
+        // Create a Pattern object
+        Pattern regex = Pattern.compile(pattern);
+
+        // Create a Matcher object
+        Matcher matcher = regex.matcher(email);
+
+        // Check if the email matches the pattern
+        return matcher.matches();
+    }
+    public void CreateAccountButton(ActionEvent event) throws IOException{
+        String firstName = FirstNameTextField.getText();
+        String email = EmailTextField.getText();
+        String password = PasswordTextField.getText();
+
+        if (firstName.length() > 3 && isValidEmail(email) && email.endsWith("@gmail.com") && password.length() > 8) {
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/capstoneproject/HomeView.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            stage.setScene(scene);
+        } else {
+            LogInMessageLabel.setText("Please enter valid credentials");
+        }
+    }
 
 }
