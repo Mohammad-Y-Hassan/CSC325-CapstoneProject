048fbf88e290a19fa9f2bbc8503664089144ad7e
mohas <mohassan11714@gmail.com>
2024-03-24 10:04:46 -0400

    There is an option to switch from log in and create account and vice versa.

--- src/main/java/com/example/capstoneproject/Main.java
+++ src/main/java/com/example/capstoneproject/Main.java
@@ -4,10 +4,39 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.cloud.FirestoreClient;
 
+import com.google.firebase.auth.*;
+import com.google.cloud.firestore.*;
+import com.google.api.core.ApiFuture;
+import java.io.FileInputStream;
+import com.google.firebase.FirebaseOptions;
+// import com.example.capstoneproject.FirestoreContext;
 public class Main extends Application {
+
+    public static Firestore fstore;
+    public static FirebaseAuth fauth;
+ //   private final FirestoreContext contxtFirebase = new FirestoreContext();
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+//        fstore = contxtFirebase.firebase();
+//        fauth = FirebaseAuth.getInstance();
+        // Initialize Firebase
+//        FileInputStream serviceAccount =
+//                new FileInputStream(String.valueOf(getClass().getResource("key.json")));
+
+//        FirebaseOptions options = new FirebaseOptions.Builder()
+//                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                .build();
+
+//        FirebaseApp.initializeApp(options);
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/capstoneproject/CreateAccount.fxml"));
 
         // Set the root element type programmatically
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,12 @@
 module com.example.capstoneproject {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires firebase.admin;
+    requires com.google.auth;
+    requires com.google.auth.oauth2;
+    requires google.cloud.firestore;
+    requires google.cloud.core;
+    requires com.google.api.apicommon;
 
     opens com.example.capstoneproject to javafx.fxml;
     exports com.example.capstoneproject;
