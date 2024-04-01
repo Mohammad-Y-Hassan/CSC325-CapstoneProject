d8d6911d47c45fabb3acbbe36a54d58457fc4396
Donovin Bermudez <donovin12131415@gmail.com>
2024-03-31 20:39:14 -0400

    Fixed bug where User tabbing during account creation was out of order
    Created a Function that returns user to Home Screen by clicking MarketMate Logo

--- src/main/java/com/example/capstoneproject/HomeViewController.java
+++ src/main/java/com/example/capstoneproject/HomeViewController.java
@@ -1,5 +1,4 @@
 package com.example.capstoneproject;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -8,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HomeViewController {
